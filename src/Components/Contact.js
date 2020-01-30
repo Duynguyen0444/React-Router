b9a8be9d4a73286353a96d84777cb7680f00bd62
duynguyen0333 <duynguyen0333@gmail.com>
2020-01-30 10:46:59 +0700

    Use Prompt to check before switch page

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,12 +1,33 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Prompt} from 'react-router-dom';
 
-
-class Contact extends Component {  
-    render() { 
-        return (  
-            <div>Contact</div>
-        );
+class Contact extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isChecked: false
     }
+  }
+  onClick = isChecked =>{
+      this.setState({
+          isChecked
+      })
+  }
+  render() {
+    var {isChecked} = this.state;
+    return (
+      <div>
+        <div>Contact</div>
+
+        <button type="button" className="btn btn-default" onClick={()=> this.onClick(false)}>Allow</button>
+        <br/>
+        <button type="button" className="btn btn-danger" onClick={()=> this.onClick(true)}>Not Allow</button>
+        <Prompt
+          when={isChecked}        
+          message={location =>(`Are you sure go to ${location.pathname}`)} />
+      </div>
+    );
+  }
 }
- 
+
 export default Contact;
